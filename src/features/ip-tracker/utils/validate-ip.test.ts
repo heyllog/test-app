1de1dedf121e332556d94a29b0399e4943984db7
V.K <heyllog@gmail.com>
2024-06-11 21:53:34 +0400

    test(validate-ip): replace it with test

--- src/features/ip-tracker/utils/validate-ip.test.ts
+++ src/features/ip-tracker/utils/validate-ip.test.ts
@@ -1,14 +1,14 @@
 import { validateIpV4 } from './validate-ip'
 
 describe('validateIpV4', () => {
-  it('validates correct IPv4 addresses', () => {
+  test('validates correct IPv4 addresses', () => {
     expect(validateIpV4('192.168.1.1')).toBe(true)
     expect(validateIpV4('255.255.255.255')).toBe(true)
     expect(validateIpV4('0.0.0.0')).toBe(true)
     expect(validateIpV4('127.0.0.1')).toBe(true)
   })
 
-  it('invalidates incorrect IPv4 addresses', () => {
+  test('invalidates incorrect IPv4 addresses', () => {
     expect(validateIpV4('256.256.256.256')).toBe(false)
     expect(validateIpV4('192.168.1.1.1')).toBe(false)
     expect(validateIpV4('192.168.1')).toBe(false)
